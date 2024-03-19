04eddf99761eafd010639b2bbb03c0d328f5e2fb
matsuzakayuuta <yuutaapuppet0628@gmail.com>
2024-03-19 19:28:11 +0900

    [Add] Home画面遷移時のアニメーションの実装06

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -5,17 +5,19 @@ import styles from "@/components/Loading/Loading.module.css";
 const Loading = () => {
   return (
     <div className={styles.loading}>
-      <div className={styles.loading_logo}>
-        <div className={styles.fadeUp}>
-          <Image
-            src="/logo.png"
-            alt="logo image"
-            width={200}
-            height={200}
-            priority
-          />
+        <div className={styles.loading_logo}>
+          <div className={styles.fadeUp}>
+            <div className={styles.fadeOut}>
+                <Image
+                  src="/logo.png"
+                  alt="logo image"
+                  width={200}
+                  height={200}
+                  priority
+                />
+            </div>
+          </div>
         </div>
-      </div>
     </div>
   );
 }
